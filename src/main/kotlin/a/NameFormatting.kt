804916f82fa0196c19ce4e990f001147fce3499f
d.kobyakov <d.kobyakov@okko.tv>
2025-01-06 14:57:33 +0300

    Added corner cases des

--- src/main/kotlin/a/NameFormatting.kt
+++ src/main/kotlin/a/NameFormatting.kt
@@ -7,7 +7,8 @@ class NameFormatting () {
 // We need to make an Initials from this names and surnames.
 
 // Examples: "Denis Lee" -> "DL", "Mark Zuckerberk" - "MZ", "Yunis Abdulzade" - "YA".
-// Remember     
+
+// Remember that initials are the first two letters of first and last name, so remember about corner-cases.
 
     // return initials
     fun getInititals(nameSurname: String): String {
