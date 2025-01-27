95f2cdf61cea4e789ba49a73f4b181f6725697c2
d.kobyakov <d.kobyakov@okko.tv>
2025-01-27 12:57:03 +0300

    Small description imroved

--- src/main/kotlin/a/NameFormatting.kt
+++ src/main/kotlin/a/NameFormatting.kt
@@ -2,13 +2,14 @@ package a
 
 class NameFormatting () {
 
-// We get a names/surnames examples from android phonebook via broadcast receiver.
+// We get a names/surnames examples from android phonebook via content provider.
 
 // We need to make an Initials from this names and surnames.
 
-// Examples: "Denis Lee" -> "DL", "Mark Zuckerberk" - "MZ", "Yunis Abdulzade" - "YA".
+// Examples: "Denis Lee" -> "DL", "Mark Zuckerberk" - "MZ", "Yunis Abdulzade" - "YA", "Rahim Fatih Abdul-Zade".
 
 // Remember that initials are the first two letters of first and last name, so remember about corner-cases.
+// And
 
     // return initials
     fun getInititals(nameSurname: String): String {
